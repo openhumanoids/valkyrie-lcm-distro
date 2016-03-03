93b0d89b066ab1e4e139c1a7ed70fd01ec887cde
Wolfgang Merkt <w.merkt@gmail.com>
2016-03-03 16:04:32 +0000

    Setup path script

---
+++ setup_path.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export DISTRO_BASE=`pwd`
+
+export PATH=$PATH:$DISTRO_BASE/build/bin
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$DISTRO_BASE/build/lib:$DISTRO_BASE/build/lib64:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$DISTRO_BASE/build/lib/pkgconfig:$DISTRO_BASE/build/lib64/pkgconfig:$PKG_CONFIG_PATH
