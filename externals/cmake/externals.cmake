098b10e6b82d2ecabc47db18f30a3fee305d031f
Wolfgang Merkt <w.merkt@gmail.com>
2016-03-15 11:46:39 +0000

    Back to OH

--- externals/cmake/externals.cmake
+++ externals/cmake/externals.cmake
@@ -7,9 +7,8 @@ set(lcm_external_args
   BUILD_IN_SOURCE 1
   )
 
-set(bot_core_lcmtypes_url https://github.com/iamwolf/bot_core_lcmtypes.git)
-#set(bot_core_lcmtypes_url https://github.com/openhumanoids/bot_core_lcmtypes.git)
-set(bot_core_lcmtypes_revision bc6398c77c7f3eee8b492d4ec76a6a6c65dfeeca)
+set(bot_core_lcmtypes_url https://github.com/openhumanoids/bot_core_lcmtypes.git)
+set(bot_core_lcmtypes_revision 3ef5f6ddc157e7ab38d4d2043538dfc14234a258)
 set(bot_core_lcmtypes_depends ${lcm_proj})
 
 set(libbot_url https://github.com/openhumanoids/libbot.git)
