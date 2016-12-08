72499adfd73eb0b5b16947d16a520c6f4cb354ca
Administrator <administrator@cpr-ued04.cpr-ued04>
2016-12-08 10:33:00 -0500

    Add Husky config (wxm)

--- setup_path.sh
+++ setup_path.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export LCM_DEFAULT_URL=udpm://239.255.76.67:7777?ttl=1
+
 export DISTRO_BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH=$PATH:$DISTRO_BASE/build/bin
